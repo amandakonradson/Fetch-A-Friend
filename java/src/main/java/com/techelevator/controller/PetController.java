85c3ebcec57efb6632ab170b0c61cdf8531f1954
Abigail Clery <abby.clery@gmail.com>
2021-09-22 10:07:00 -0400

    Pushing change to PetController

--- java/src/main/java/com/techelevator/controller/PetController.java
+++ java/src/main/java/com/techelevator/controller/PetController.java
@@ -3,6 +3,7 @@ package com.techelevator.controller;
 import com.techelevator.dao.PetDao;
 import com.techelevator.model.Pet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +16,7 @@ public class PetController {
     @Autowired
     private PetDao petDao;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/addPuppy")
     public void createPet(@RequestBody Pet newDog){
         petDao.createPet(newDog);
